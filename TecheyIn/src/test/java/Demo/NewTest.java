6cd933cdea0341013896728ff3975dc9f0329382
praveen kumar J <praveen kumar J@DESKTOP-C9H20OP>
2018-02-24 18:23:07 +0530

    Adding Extent Report Functionality

--- TecheyIn/src/test/java/Demo/NewTest.java
+++ TecheyIn/src/test/java/Demo/NewTest.java
@@ -7,29 +7,45 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+
+import extreports.ExtentManager;
+
 public class NewTest {
 	
+	public static ExtentReports report;
+	public static ExtentTest logger;
 	public WebDriver driver;
 	
 	@Test(priority=0)				
 	public void testEasy() {	
+		
 		driver.get("http://demo.guru99.com/test/guru99home/");  
 		String title = driver.getTitle();				 
 		Assert.assertTrue(title.contains("Demo Guru99 Page")); 	
 		System.out.println("The System has Successfully checked the Bank Page");
+		logger=report.createTest("WebSite Guru99 Page", "Check for Title of the page");
+		logger.pass("Test Passed Successfully");
 	}	  
 	
 	@Test(priority=1)
 	public void TestPrint() {
 		System.out.println("The System has Successfully Finsihed First Jenkins Integration Project");
+		logger.log(Status.FAIL, "Checking Fail Method");
+		//logger.fail("Failed Method Verification");
 	}
 	@BeforeTest
 	public void beforeTest() {	
 	    System.setProperty("webdriver.chrome.driver", "C:\\Users\\praveen kumar J\\Documents\\ChromeDriver\\chromedriver.exe");
 		driver = new ChromeDriver();  
+		report=ExtentManager.GetExtent();
+		
 	}		
 	@AfterTest
 	public void afterTest() {
-		driver.quit();			
+		report.flush();
+		driver.quit();
 	}		
 }
