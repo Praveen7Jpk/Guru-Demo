da800518ba8b0a0e2decb3562e230bebdde72307
praveen kumar J <praveen kumar J@DESKTOP-C9H20OP>
2018-02-24 15:13:37 +0530

    Jenkin Integration Test

--- TecheyIn/src/test/java/Demo/NewTest.java
+++ TecheyIn/src/test/java/Demo/NewTest.java
@@ -21,7 +21,7 @@ public class NewTest {
 	
 	@Test(priority=1)
 	public void TestPrint() {
-		System.out.println("The System has Successfully Finsihed First All Integration Project");
+		System.out.println("The System has Successfully Finsihed First Jenkins Integration Project");
 	}
 	@BeforeTest
 	public void beforeTest() {	
