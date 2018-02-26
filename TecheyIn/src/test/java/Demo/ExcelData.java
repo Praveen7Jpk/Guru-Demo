7013a7c8bccb1f587697ce93e273a08905050f44
praveen kumar J <praveen kumar J@DESKTOP-C9H20OP>
2018-02-26 22:54:28 +0530

    Adding Apache POI to Read/Write Excel Files

--- TecheyIn/src/test/java/Demo/ExcelData.java
+++ TecheyIn/src/test/java/Demo/ExcelData.java
@@ -1,10 +1,12 @@
 package Demo;
 
-public class bet {
+public class ExcelData {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("The thing is Maven Test");
+ 
+		
+		
 	}
 
 }
